a4390d3b18b54b2c7193a2654a2606e4ed7a75f9
Salim Ramirez <salimramirezm@gmail.com>
2025-07-12 17:39:39 -0500

    feat(scm): add event handler for MinimumQuantityThresholdReachedEvent.java

---
+++ src/main/java/com/blueyonder/platform/u20201e843/scm/application/internal/eventhandlers/MinimumQuantityThresholdReachedEventHandler.java
@@ -0,0 +1,20 @@
+package com.blueyonder.platform.u20201e843.scm.application.internal.eventhandlers;
+
+import com.blueyonder.platform.u20201e843.scm.domain.model.events.MinimumQuantityThresholdReachedEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MinimumQuantityThresholdReachedEventHandler {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MinimumQuantityThresholdReachedEventHandler.class);
+
+    @EventListener
+    public void on(MinimumQuantityThresholdReachedEvent event) {
+        LOGGER.info("SCM: A supply order is needed for the product with SKU {} with at least {} units",
+                event.getSkuIdentifier().skuIdentifier(),
+                event.getRequestedSupplyQuantity());
+    }
+}
