5eb1f64d41206bd6c38af30becac328de65b27f4
Salim Ramirez <salimramirezm@gmail.com>
2025-07-12 15:31:03 -0500

    feat(scm): update InventoryItem with constructor

--- src/main/java/com/blueyonder/platform/u20201e843/scm/domain/model/aggregates/InventoryItem.java
+++ src/main/java/com/blueyonder/platform/u20201e843/scm/domain/model/aggregates/InventoryItem.java
@@ -2,12 +2,52 @@ package com.blueyonder.platform.u20201e843.scm.domain.model.aggregates;
 
 import com.blueyonder.platform.u20201e843.scm.domain.model.valueobjects.InventoryItemStatus;
 import com.blueyonder.platform.u20201e843.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
+import com.blueyonder.platform.u20201e843.shared.domain.model.valueobjects.SkuIdentifier;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
 @Entity
 public class InventoryItem extends AuditableAbstractAggregateRoot<InventoryItem> {
 
+    @Embedded
+    @NotNull
+    @Column(nullable = false)
+    private SkuIdentifier skuIdentifier;
+
+    @NotNull
+    @Column(nullable = false)
     private InventoryItemStatus status;
+
+    @NotNull
+    @Column(nullable = false)
+    private Double minimumQuantity;
+
+    @Setter
+    @NotNull
+    @Column(nullable = false)
+    private Double availableQuantity;
+
+    @Setter
+    @Column(nullable = false)
+    private Double reservedQuantity;
+
+    @Setter
+    @Column(nullable = false)
+    private Double pendingSupplyQuantity;
+
+    public InventoryItem() {}
+
+    public InventoryItem(String skuIdentifier, Double minimumQuantity,  Double availableQuantity) {
+        this.skuIdentifier = new SkuIdentifier(skuIdentifier);
+        this.minimumQuantity = minimumQuantity;
+        this.availableQuantity = availableQuantity;
+        this.status = InventoryItemStatus.WITH_STOCK;
+        this.reservedQuantity = 0.0;
+        this.pendingSupplyQuantity = 0.0;
+    }
 }
